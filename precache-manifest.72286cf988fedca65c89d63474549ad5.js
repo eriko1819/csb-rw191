360e9b5950c53430eb42c4d5928ac198b949013d
Code Sandbox <hello@codesandbox.io>
2022-02-07 04:11:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.72286cf988fedca65c89d63474549ad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6168b88a5246d71f07465570fd48a395",
+    "url": "https://eriko1819.github.io/csb-rw191/index.html"
+  },
+  {
+    "revision": "368b9a775840f51e4a4f",
+    "url": "https://eriko1819.github.io/csb-rw191/static/css/main.af41b280.chunk.css"
+  },
+  {
+    "revision": "c313cff00cb4a23954e9",
+    "url": "https://eriko1819.github.io/csb-rw191/static/js/2.2eb88444.chunk.js"
+  },
+  {
+    "revision": "368b9a775840f51e4a4f",
+    "url": "https://eriko1819.github.io/csb-rw191/static/js/main.69573e53.chunk.js"
+  },
+  {
+    "revision": "55b1b526155a7f027259",
+    "url": "https://eriko1819.github.io/csb-rw191/static/js/runtime~main.7e04c8ab.js"
+  }
+]);
